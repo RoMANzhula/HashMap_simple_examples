c490f7edfbfdecf57c49f0f5f0e1e5cad4191880
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:39:28 +0200

    Update Main.java
    Remove comments

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        removeByValueFromHashMap(); //вызываем метод, который удалит из HashMap все элементы, у которых значение будет
-        //меньше, чем введет пользователь
+        removeByValueFromHashMap();
     }
 
-    public static void removeByValueFromHashMap() { //метод, который удаляет из HashMap все элементы, у которых значение
-        //меньше, чем введет пользователь
-        System.out.println("Please input number for delete elements that are less than: \s"); //просим пользователя ввести число
-        Scanner scanner = new Scanner(System.in); //с помощью класса Scanner
-        int numberFromConsole = scanner.nextInt(); //создаем переменную, в которую читаем введенное пользователем число с клавиатуры
+    public static void removeByValueFromHashMap() { 
+        System.out.println("Please input number for delete elements that are less than: \s");
+        Scanner scanner = new Scanner(System.in);
+        int numberFromConsole = scanner.nextInt();
 
-        HashMap<String, Integer> ages = new HashMap<String, Integer>(); //создаем коллекцию с типом и поведением HashMap с
-        //параметрами(дженериками) <String, Integer>
-        //заполняем нашу коллекцию данными
+        HashMap<String, Integer> ages = new HashMap<String, Integer>();
+        
         ages.put("Mykola", 22);
         ages.put("Sergiy", 23);
         ages.put("Vasyl", 32);
@@ -24,19 +21,17 @@ public class Main {
         ages.put("Maria", 24);
         ages.put("Foontic", 28);
 
-        String[] arrayKeys = new String[ages.size()]; //создаем строковый массив для Ключей(т.к. наш первый дженерик в Карте String),
-        //который имеет длину равную размеру нашей Карты(коллекции)
-        arrayKeys = ages.keySet().toArray(arrayKeys); //заполняем массив элементами - Ключами из карты
+        String[] arrayKeys = new String[ages.size()];
+        arrayKeys = ages.keySet().toArray(arrayKeys);
 
-        for (String elementArrayKeys : arrayKeys) { //с помощью for-each проходим по каждому элементу из массива
-            if (ages.get(elementArrayKeys) < numberFromConsole) { //и если элемент Карты по Ключу(элемент массива) имеет
-                //Значение меньше, чем ввел пользователь на клавиатуре, то
-                ages.remove(elementArrayKeys);//мы удаляем из Карты данный элемент
+        for (String elementArrayKeys : arrayKeys) {
+            if (ages.get(elementArrayKeys) < numberFromConsole) {
+                ages.remove(elementArrayKeys);
             }
         }
 
         System.out.println("Our collection after deleted by value: " + ages);
 
-        scanner.close(); //очищаем ресурсы
+        scanner.close();
     }
 }
